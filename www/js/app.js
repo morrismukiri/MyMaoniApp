0bdb8e0e3f9ec5441d2e5987571aace509691221
Morris Mukiri <morrismukiri@gmail.com>
2017-04-12 18:41:47 +0300

    ionic-dateicker

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers' , 'starter.services','starter.routes', 'ionic-material','angularMoment'])
+angular.module('starter', ['ionic', 'starter.controllers' , 'starter.services','starter.routes',
+ 'ionic-material','angularMoment','ionic-datepicker'])
 
 .run(function($ionicPlatform , $rootScope, $timeout) {
   $ionicPlatform.ready(function() {
