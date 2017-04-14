7240c2bc7faf967ee757f50a249dd71dd63c5495
Morris Mukiri <morrismukiri@gmail.com>
2017-04-14 22:31:54 +0300

    Test url

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,15 +1,16 @@
 angular.module('starter')
 
-.constant('AUTH_EVENTS', {
-  notAuthenticated: 'auth-not-authenticated',
-  notAuthorized: 'auth-not-authorized'
-})
+  .constant('AUTH_EVENTS', {
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+  })
 
-.constant('USER_ROLES', {
-  admin: 'admin_role',
-  public: 'public_role'
-})
+  .constant('USER_ROLES', {
+    admin: 'admin_role',
+    public: 'public_role'
+  })
 
-.constant('API',{
-  root:'http://localhost:8000/api/v1/'
-});
+  .constant('API', {
+    // root:'http://localhost:8000/api/v1/'
+    root: 'http://40.10.10.192/mymaoniback/public/index.php/api/v1/'
+  });
