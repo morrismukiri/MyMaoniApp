c6550f930238f0421b5f4f08671236500870142e
Morris Mukiri <morrismukiri@gmail.com>
2017-04-24 09:20:06 +0300

    My Polls view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -348,11 +348,35 @@ angular.module('starter.controllers', [])
 
   })
 
-  .controller('pollsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-    // You can include any angular dependencies as parameters for this function
-    // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
+  .controller('pollsCtrl', ['$scope', '$stateParams', 'API', '$http', 'AuthService',
+  function ($scope, $stateParams, API, $http, AuthService) {
 
+      $http.get(API.root + "pollsbyuser/"+AuthService.userId).then(
+        function (result) {
+          $scope.polls = result.data.data;
+
+          console.log(result.data.message, $scope.polls);
+        },
+        function (response) {
+          console.log(response);
+        }
+      )
+      $scope.doRefresh = function () {
+        $http.get(API.root + "pollsbyuser/"+AuthService.userId).then(
+          function (result) {
+            $scope.polls = result.data.data;
+
+            console.log(result.data.message, $scope.polls);
+          },
+          function (response) {
+            console.log(response);
+          }
+        )
+          .finally(function () {
+            // Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+          });
+      };
 
     }])
 
