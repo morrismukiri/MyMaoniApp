f94535d7bee8af300cdf77d6a251ba22a608068b
Morris Mukiri <morrismukiri@gmail.com>
2017-07-07 14:09:36 +0300

    Build production

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
  cordova build --release android
- jarsigner -verbose  -sigalg SHA1WITHRSA -digestalg SHA1 -keystore 'D:\Dev\keys\Truck Weight\keyStore.jks' .\platforms\and
-roid\build\outputs\apk\android-release-unsigned.apk mineco
+ jarsigner -verbose  -sigalg SHA1WITHRSA -digestalg SHA1 -keystore 'D:\Dev\keys\Truck Weight\keyStore.jks' .\platforms\android\build\outputs\apk\android-release-unsigned.apk mineco
 
-  zipalign -v 4 .\platforms\android\build\outputs\apk\android-release-unsigned.apk com.mymaoni.app.apk
+  zipalign -f -v 4 .\platforms\android\build\outputs\apk\android-release-unsigned.apk com.mymaoni.app.apk
