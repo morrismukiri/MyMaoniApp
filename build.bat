880ade5ef2d5441f2c6101887c8a42eb4a7901e4
Morris Mukiri <morrismukiri@gmail.com>
2017-09-14 23:31:23 +0300

    release build and keystore

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
  cordova build --release android
 
+ jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore 'D:\Dev\keys\cromemedia_release.keystore' .platforms\android\build\outputs\apk\android-release-unsigned.apk cromemedia
+
  jarsigner -verbose  -sigalg SHA1WITHRSA -digestalg SHA1 -keystore 'D:\Dev\keys\Truck Weight\keyStore.jks' .platforms\android\build\outputs\apk\android-release-unsigned.apk mineco
 
   zipalign -f -v 4 .\platforms\android\build\outputs\apk\android-release-unsigned.apk com.mymaoni.app.apk
